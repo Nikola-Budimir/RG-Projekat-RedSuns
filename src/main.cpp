a404ef3763cfdae9b19fb751980d539be6edd545
Nikola-Budimir <mi21178@alas.matf.bg.ac.rs>
2024-04-10 21:01:38 +0200

    added faceculling

--- src/main.cpp
+++ src/main.cpp
@@ -185,6 +185,12 @@ int main() {
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+    //enabling faceculling
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CW);
+
+
 
     //buffering of vertexes and loading textures
 
